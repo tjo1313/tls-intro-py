d878a1d585b83580e571bd1a8b3f770ddd8e4d38
TJ O'Connor <tjoconnor13@gmail.com>
2024-02-11 11:17:15 -0700

    Add Ch. 1 exercises

---
+++ ch1/exercise.py
@@ -0,0 +1,30 @@
+# Create a tuple called 'names' that contains several pet names. For instance:
+
+# Asta
+# Butterscotch
+# Pudding
+# Neptune
+# Darwin
+
+names = (
+    'Asta',
+    'Butterscotch',
+    'Pudding',
+    'Neptune',
+    'Darwin',
+)
+
+print(names)
+
+# Create a dictionary named 'pets' that contains a list of pet names and the 
+# type of the animal.
+
+pets = {
+    'Asta':         'dog',
+    'Butterscotch': 'cat',
+    'Pudding':      'cat',
+    'Neptune':      'fish',
+    'Darwin':       'turtle,'
+}
+
+print(pets)
