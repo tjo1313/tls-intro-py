133cedf68a09dd7b7524132530f3093e9fdd28f9
TJ O'Connor <tjoconnor13@gmail.com>
2024-02-11 13:33:56 -0700

    Add Basic Operations exercise

---
+++ basic-operations/exercise.py
@@ -0,0 +1,10 @@
+# Concatenate two strings
+
+print("T.J. " + "O'Connor")    # T.J. O'Connor
+
+# Use coercion to print '15' instead of '510'
+
+print('5' + '10')    #510
+print(int('5') + int('10'))    #15
+
+# print(int('3.1415'))    ValueError
