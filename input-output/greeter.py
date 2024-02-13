744c0bb353c981de69e92ec351b5ac12ed45b2d5
TJ O'Connor <tjoconnor13@gmail.com>
2024-02-12 20:47:27 -0700

    Add I/O exercises

---
+++ input-output/greeter.py
@@ -0,0 +1,14 @@
+# This program should ask for your name, then output "Hello, NAME!" where NAME
+# is the name you entered.
+
+#name = input("What is your name?\n")
+#print(f'Hello, {name}!')
+
+# Modify the above code above to ask for user's first and last names separately
+# then greet the user with their full name.
+
+first_name = input("What is your first name?\n")
+last_name = input ("What is your last name?\n")
+
+print(f'Hello, {first_name} {last_name}!')
+
